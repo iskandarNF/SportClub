690b90c56922b9298dd6e844b3198f5b7630d0f6
i.nizomov <i.nizomov@spitamen.com>
2020-04-20 15:59:30 +0500

    Initial commit

---
+++ app/src/main/java/tj/stamenbank/sportclub/AddMember.java
@@ -0,0 +1,14 @@
+package tj.stamenbank.sportclub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_member);
+    }
+}
